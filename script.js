5b4c11cd8ad9198ad387f4292e29f1f6a9db315c
Daniel Galdino <danielgaldino1712@gmail.com>
2021-01-18 11:54:51 -0300

    commit-footer

---
+++ script.js
@@ -0,0 +1,11 @@
+// if(Number(window.innerWidth) <= 1000) {
+//     let sec4_art1 = document.querySelector('.sec-4_art-1')
+//     let moldura = document.querySelector('.moldura')
+//     sec4_art1.removeChild(moldura)
+// } else {
+//     console.log('deu errado', Number(window.innerWidth))
+// }
+
+
+
+
