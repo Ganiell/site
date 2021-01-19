5de820758b44a1f297a4f64a54ab880a24fb5daf
Daniel Galdino <danielgaldino1712@gmail.com>
2021-01-19 14:11:34 -0300

    commit-text

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-// if(Number(window.innerWidth) <= 1000) {
-//     let sec4_art1 = document.querySelector('.sec-4_art-1')
-//     let moldura = document.querySelector('.moldura')
-//     sec4_art1.removeChild(moldura)
-// } else {
-//     console.log('deu errado', Number(window.innerWidth))
-// }
+let string = ''
+let form = document.querySelector('form')
+
+form.addEventListener('submit', function(evento) {
+    evento.preventDefault()
+    let item
+    for(let i = 0; i < this.elements.length - 1; i++) {
+        item = this.elements[i]
+        string = item.value
+        string = string.trim()
+        if(!string) {
+            item.classList.add('erro')
+        } else {
+            item.classList.remove('erro')
+            item.classList.remove('erro2')
+        }
+    }
+})
 
 
-
+for(let i of form.elements) {
+    if(i.type !== 'submit') {
+        i.addEventListener('keyup', function(){
+            string = i.value
+            string = string.trim()
+            if(!string){
+                i.value = ''
+                i.setAttribute('placeholder', 'Preencha este campo')
+                i.classList.add('erro2')
+            } else {
+                i.classList.remove('erro')
+                i.classList.remove('erro2')
+            }
+        })
+    }
+}
 
